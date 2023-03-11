8f073ca2415a9ac1bb851b119b303af9c55b8778
Marco Segura <87828147+marcosegura1@users.noreply.github.com>
2023-03-11 23:40:27 +0100

    updateed

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const app = express();
-const db = require('mysql');
+// server.js -> it's your local api
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  port = process.env.PORT || 4000;
 
-// Crear la conexión a la base de datos
-const connection = db.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'users'
+// enable CORS without external module
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.get('/profile', (req, res) => {
-  // hacer una consulta a la base de datos para obtener los valores profile_photo, title, description, address, labels
-  connection.query('SELECT profile_photo, title, description, address, labels FROM users', (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// request handlers
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Enable CORS' });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log('Server started on: ' + port);
+});
